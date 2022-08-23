8ab3a4e629905763de746e28aa576821e2ed37c1
Wibowo, Alex <Alex.Wibowo@anz.com>
2022-08-23 23:04:28 +1000

    Exercise 5: separating container component and presentation component

--- 05/demos/demos/before/public/js/main.js
+++ 05/demos/demos/before/public/js/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import Conversion from './components/conversion.js';
+import Conversion from './containers/Conversion.js';
 import store from './stores/configureStore'; 
 
 
