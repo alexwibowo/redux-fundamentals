5e233f81967f45646757f4a30d6e416c7052a09e
Wibowo, Alex <Alex.Wibowo@anz.com>
2022-08-24 20:33:41 +1000

    Exercise 6: activating redux-logger in middleware

--- 06/demos/demos/before/public/js/stores/configureStore.js
+++ 06/demos/demos/before/public/js/stores/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 
 var defaultState = {
     originAmount: '0.00'
@@ -16,6 +17,9 @@ function amount(state = defaultState, action) {
     return state;
 }
 
-var store = createStore(amount);
+var store = createStore(
+    amount,
+    applyMiddleware(logger)
+);
 
 export default store;
