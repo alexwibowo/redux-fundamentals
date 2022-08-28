95aedcec90aaf780314b0d72af0d3386a79acc5d
Wibowo, Alex <Alex.Wibowo@anz.com>
2022-08-28 23:11:26 +1000

    Exercise 7: removing unused codes

--- 07/demos/demos/before/public/js/containers/Conversion.js
+++ 07/demos/demos/before/public/js/containers/Conversion.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
-import debounce from 'lodash.debounce';
+
 
 import FeesTable from '../components/FeesTable';
 import * as actions from '../actions/actions';
@@ -9,31 +8,15 @@ import * as actions from '../actions/actions';
 class Conversion extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            // originAmount: '0.00',
-            // originCurrency: 'USD',
-            // destinationAmount: '0.00',
-            // destinationCurrency: 'EUR',
-            feeAmount: 0.00,
-            // conversionRate: 1.5,
-            totalCost: 0.00
-          //  errorMsg: ''
-        }
 
         // bind event listeners so 'this' will be available in the handlers
         this.handleOriginAmountChange = this.handleOriginAmountChange.bind(this);
         this.handleDestAmountChange = this.handleDestAmountChange.bind(this);
         this.handleOriginCurrencyChange = this.handleOriginCurrencyChange.bind(this);
         this.handleDestCurrencyChange = this.handleDestCurrencyChange.bind(this);
-    //    this.handleAjaxFailure = this.handleAjaxFailure.bind(this);
     }
 
     componentDidMount() {
-        // Add a debounced version of _getDestinationAmount() so we avoid server & UI Thrashing.
-        // See http://stackoverflow.com/questions/23123138/perform-debounce-in-react-js/28046731#28046731
-        this.makeConversionAjaxCall = debounce(this._makeConversionAjaxCall, 350);
-        this.makeFeeAjaxCall = debounce(this._makeFeeAjaxCall, 350);
-
         this.originAmountInput.focus();
     }
 
@@ -82,49 +65,7 @@ class Conversion extends React.Component {
         this.props.dispatch(actions.fetchFees(feePayload));
     }
 
-    handleCurrencyChange(currentlyEditing, event) {
-        var obj = {};
-        if (currentlyEditing === 'origin') {
-            obj.originCurrency = event.target.value
-        } else {
-            obj.destinationCurrency = event.target.value
-        }
-
-        // just change both...
-        // we have to use the callback so `this.state` will reflect the proper values
-        // when they are called in _makeConversionAjaxCall()
-        this.setState(obj, () => {
-            // get new dest amount & conversion rates
-            this.makeConversionAjaxCall({}, (resp) => {
-                this.clearErrorMessage();
-
-                this.setState({
-                    originAmount: resp.originAmount,
-                    // destinationAmount: resp.destAmount,
-                    destinationAmount: this.props.destinationAmount,
-                    conversionRate: resp.xRate
-                });
-
-                // get the new fee & total amount
-                this.makeFeeAjaxCall({
-                    originAmount: resp.originAmount,
-                    originCurrency: this.props.originCurrency,
-                    destCurrency: this.props.destinationCurrency
-
-                }, (response) => {
-                    this.setState({
-                        feeAmount: response.feeAmount
-                    })
-
-                    this.calcNewTotal();
-                }, this.handleAjaxFailure);
-            });
-
-
-        })
-
-
-    }
+   
     handleOriginAmountChange(event) {
         var newAmount = event.target.value;
 
@@ -150,7 +91,6 @@ class Conversion extends React.Component {
         }
 
         this.props.dispatch(actions.fetchFees(feePayload));
-
     }
 
     handleDestAmountChange(event) {
@@ -172,51 +112,7 @@ class Conversion extends React.Component {
         this.props.dispatch(actions.fetchConversionRateAndFees(payload));
     }
 
-   
-    // this is debounced in `componentDidMount()` as this.makeConversionAjaxCall()
-    _makeConversionAjaxCall(data, successCallback, failureCallback) {
-        var originCurrency = this.props.originCurrency;
-        var destCurrency = this.props.destinationCurrency;
-
-        var payload = {
-            originAmount: data.newValue || this.props.originAmount,
-            destAmount: data.newValue || this.state.destAmount,
-            originCurrency: originCurrency,
-            destCurrency: destCurrency,
-            calcOriginAmount: false
-        }
-
-        // determine whether we need to calc origin or dest amount
-        if (data.currentlyEditing === 'dest') {
-            payload.calcOriginAmount = true
-        }
-
-        // ajax call for destination amount
-        // originCurrency, destCurrency, originAmount
-        axios.get('/api/conversion', {
-            params: payload
-        })
-        .then((resp) => {
-            successCallback(resp.data);
-        })
-        .catch(failureCallback);
-
-    }
-    // this is debounced in `componentDidMount()`
-    _makeFeeAjaxCall(payload, successCallback, failureCallback) {
-        axios.get('/api/fees', {
-            params: payload
-        })
-        .then((resp) => {
-            successCallback(resp.data);
-        })
-        .catch(failureCallback);
-    }
-    calcNewTotal() {
-        var newTotal = parseFloat(this.props.originAmount, 10) + parseFloat(this.state.feeAmount, 10);
-        this.setState({ totalCost: parseFloat(newTotal) });
-    }
-
+    
     render() {
 
         if (this.props.errorMsg) {
